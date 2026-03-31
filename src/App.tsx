148510005465c3b071a10873af92fd4904368e66
Thang Tuan Tran <thangtran081204@gmail.com>
2026-03-31 18:22:08 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">CineMax 🎬</h1>
+    </div>
+  )
+}
+
+export default App
